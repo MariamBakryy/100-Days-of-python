e6bce4fe71fcd6e951f9eda2986b092e1da7452f
MariamBakry <141127911+MariamBakryy@users.noreply.github.com>
2023-11-11 00:35:52 +0200

    Day 1

---
+++ Day 1.py
@@ -0,0 +1,23 @@
+print("Hello " + input("What is your name? ") + " !")
+
+
+user_name = input("What is your name? ")
+print(user_name , " has " , len(user_name) , " characters")
+
+
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+a = a+b
+b = a-b
+a = a-b
+print("a = ", a, "\nb = ", b)
+
+
+
+
+#The Day 1 Task
+
+print("Welcome to the Band Name Generator.")
+user_city = input("What's name of the city you grew up in?\n")
+user_pet_name = input("What's your pet's name?\n")
+print("Your band name could be " + user_city + " " + user_pet_name)
